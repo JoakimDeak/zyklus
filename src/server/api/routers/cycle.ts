9b50e00131af7025ba6fdc5fa905ce9b57689cac
opera-joakimd <joakimd@opera.com>
2025-05-04 12:05:36 +0200

    Nuke biome

--- src/server/api/routers/cycle.ts
+++ src/server/api/routers/cycle.ts
@@ -1,23 +1,21 @@
-import { z } from 'zod'
-
 import { createTRPCRouter, protectedProcedure } from '~/server/api/trpc'
 
 export const cycleRouter = createTRPCRouter({
-	create: protectedProcedure.mutation(async ({ ctx }) => {
-		return ctx.db.cycle.create({
-			data: {
-				userId: ctx.session.user.id
-			}
-		})
-	}),
+  create: protectedProcedure.mutation(async ({ ctx }) => {
+    return ctx.db.cycle.create({
+      data: {
+        userId: ctx.session.user.id
+      }
+    })
+  }),
 
-	getCurrent: protectedProcedure.query(async ({ ctx }) => {
-		const cycle = await ctx.db.cycle.findFirst({
-			orderBy: {
-				startDate: 'desc'
-			}
-		})
+  getCurrent: protectedProcedure.query(async ({ ctx }) => {
+    const cycle = await ctx.db.cycle.findFirst({
+      orderBy: {
+        startDate: 'desc'
+      }
+    })
 
-		return cycle ?? null
-	})
+    return cycle ?? null
+  })
 })
